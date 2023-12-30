baa1acd87c8451fc47d8a1b86c5b2f7733250963
ParvezHossainSakib <pervezhossain1998@gmail.com>
2023-12-30 13:44:45 +0600

    changes done

--- src/Component/Shared/Footer/SubFooter.js
+++ src/Component/Shared/Footer/SubFooter.js
@@ -4,8 +4,8 @@ const SubFooter = () => {
     return (
         <div className='bg-[#002060] p-3'>
             <div className='container mx-auto flex flex-col lg:flex-row justify-between items-center text-white'>
-                    <p className='text-[10px] lg:text-[16px] mb-[8px] lg:mb-[0px]'>© Copyright MindWorksBd | All Rights Reserved.</p>
-                    <p className='text-[10px] lg:text-[16px]'>Website developed by <a href="https://www.facebook.com/pervez.me/">Md. Parvez Hossain</a></p>
+                    <p className='text-[10px] lg:text-[16px] mb-[8px] lg:mb-[0px]'>© Copyright I2IGateWay | All Rights Reserved.</p>
+                    <p className='text-[10px] lg:text-[16px]'>Website developed by <a href="https://www.facebook.com/softimize">SotimizeTech</a></p>
               </div> 
         </div>
     );
